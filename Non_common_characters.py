d10e32bba7c5ab5b50e1d1c91d7f7d5b3f2e2ef2
21MH1A05A6 <97579846+21MH1A05A6@users.noreply.github.com>
2022-07-03 13:02:51 +0530

    n=input() m=input() s1=n.lower() s2=m.lower() b=[] for i in s1:     if i not in s2 and i not in b and i!=' ':         b+=[i] for k in s2:     if k not in s1 and k not in b:         b+=[k] print(len(b))

---
+++ Non_common_characters.py
@@ -0,0 +1,12 @@
+n=input()
+m=input()
+s1=n.lower()
+s2=m.lower()
+b=[]
+for i in s1:
+    if i not in s2 and i not in b and i!=' ':
+        b+=[i]
+for k in s2:
+    if k not in s1 and k not in b:
+        b+=[k]
+print(len(b))
