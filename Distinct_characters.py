48d1a848db4be65122f9d762e060c50eb9121cc0
21MH1A05A6 <97579846+21MH1A05A6@users.noreply.github.com>
2022-07-03 13:19:25 +0530

    n=input() x=set(n.lower()) p=sorted(x) for i in p:     if i!=' ':         print(i,end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,6 @@
 n=input()
-x=list(n)
-s=''
-for i in x:
-    if  i in 'abcdefghijklmnopqrstuvwxyz' and x.count(i)==1:
-        s+=i
-p=sorted(s)
-for j in p:
-    print(j,end='')
+x=set(n.lower())
+p=sorted(x)
+for i in p:
+    if i!=' ':
+        print(i,end='')
